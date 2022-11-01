e108af60bbd7c97c60ce742324153afdb5084139
najib <81011410+narujib@users.noreply.github.com>
2022-11-01 13:23:37 +0700

    gameover

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,7 @@ const SPEED = 1500
 const GRAVITY = 200
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 3500
+const WORLD_LIMIT = 3000
 
 signal animate
 
@@ -17,6 +18,8 @@ func _physics_process(delta):
 	move_and_slide(motion, UP)
 	
 func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if is_on_floor():
 		motion.y = 0
 	elif is_on_floor():
@@ -38,4 +41,7 @@ func move():
 		
 func animate():
 	emit_signal("animate", motion)
+	
+func end_game():
+	get_tree().change_scene("res://Levels/GameOver.tscn")
 
